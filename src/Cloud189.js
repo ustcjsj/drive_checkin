6a614b5c82c87c38295e9b39e410ffe93cc6d7ba
Jiang Shunjia <jiangshunjia@outlook.com>
2025-08-23 17:43:49 +0800

    Update Cloud189.js

--- src/Cloud189.js
+++ src/Cloud189.js
@@ -171,6 +171,6 @@ const main = async () => {
     logger.log("\n\n");
     const events = recording.replay();
     const content = events.map((e) => `${e.data.join("")}`).join("  \n");
-    push("天翼云盘自动签到任务", content);
+    push("天翼云盘自动签到任务", logs + content);
   }
 })();
