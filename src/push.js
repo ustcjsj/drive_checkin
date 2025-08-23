718916aa5de2b0834c288eb73a21be7fc7c1e01c
Jiang Shunjia <jiangshunjia@outlook.com>
2025-08-23 17:44:46 +0800

    Update push.js

--- src/push.js
+++ src/push.js
@@ -89,6 +89,8 @@ const pushWxPusher = (title, desp) => {
 
 const push = (title, desp) => {
   pushTelegramBot(title, desp);
+  pushServerChan(title, desp);
+  pushWxPusher(title, desp);
 };
 
 exports.push = push;
